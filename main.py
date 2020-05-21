93adcff47cb04f8dfbd3f2f2bc0744147f5de0cb
Miguel Angel Pimentel <pime@outlook.com>
2020-05-21 14:34:03 -0500

    Inital

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""  This script is to do a recursive least square method comparation """
+
+__author__ = '{Miguel Angel Pimentel Vallejo}'
+__email__ = '{miguel.pimentel@umich.mx}'
+__date__= '{18/may/2020}'
